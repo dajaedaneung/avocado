9529b35866cf704001755aee277285c873804f80
gimhanul <gimhanuly@gmail.com>
2023-01-11 13:04:55 +0900

    project init

---
+++ src/test/java/com/djdn/warning/WarningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.djdn.warning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
